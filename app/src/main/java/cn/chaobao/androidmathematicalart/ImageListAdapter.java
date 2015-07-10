bd6159e3d0bed35d70ab2ff561e2153810147317
Chaoba <qqliys@126.com>
2015-07-10 14:30:48 +0800

    add list to show pictures

---
+++ app/src/main/java/cn/chaobao/androidmathematicalart/ImageListAdapter.java
@@ -0,0 +1,132 @@
+package cn.chaobao.androidmathematicalart;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.util.Log;
+import android.util.LruCache;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by Liyanshun on 2015/7/7.
+ */
+public class ImageListAdapter extends BaseAdapter {
+    private static final String TAG = "AccidentTypeAdapter";
+    private int mCount;
+    private Context mContext;
+    private LruCache<Integer, Bitmap> cache = new LruCache<Integer, Bitmap>(8 * 1024 * 1024) {
+        @Override
+        protected void entryRemoved(boolean evicted, Integer key, Bitmap oldValue, Bitmap newValue) {
+            super.entryRemoved(evicted, key, oldValue, newValue);
+            Log.i(TAG, "revmoved:" + key);
+            if (evicted && oldValue != null) {
+                oldValue.recycle();
+            }
+        }
+
+        @Override
+        protected int sizeOf(Integer key, Bitmap value) {
+            int size = value.getByteCount();
+            Log.i(TAG, "size:" + size);
+            return size;
+        }
+    };
+
+    public ImageListAdapter(Context c, int count) {
+        mCount = count;
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mCount;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        Log.i(TAG, "Getview:" + position);
+        final ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = View.inflate(mContext, R.layout.list_item, null);
+            holder.mImg = (ImageView) convertView.findViewById(R.id.img);
+            holder.mTxt = (TextView) convertView.findViewById(R.id.time);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        Bitmap bm = cache.get(position);
+        if (bm != null) {
+            Log.i(TAG, "has bitmap");
+            holder.mImg.setImageBitmap(bm);
+            holder.mTxt.setText("");
+        } else {
+            holder.mImg.setImageResource(R.mipmap.ic_launcher);
+            Observable observable = Observable.create(new Observable.OnSubscribe<Long>() {
+                @Override
+                public void call(Subscriber<? super Long> subscriber) {
+                    long startTime = System.currentTimeMillis();
+                    Bitmap bm = MathematicalArt.jniGetMathematicalArt(Environment.getExternalStorageDirectory()
+                            + File.separator + "MathPic.jpg", position);
+                    cache.put(position, bm);
+                    subscriber.onNext(startTime);
+                    subscriber.onCompleted();
+                }
+            });
+            observable
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeOn(Schedulers.io())
+                    .subscribe(new Subscriber<Long>() {
+
+
+                        @Override
+                        public void onCompleted() {
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                        }
+
+                        @Override
+                        public void onNext(Long startTime) {
+                            Log.i(TAG, "onNext");
+                            Bitmap bm = cache.get(position);
+                            if (bm != null) {
+                                holder.mImg.setImageBitmap(bm);
+                            }
+                            long endTime = System.currentTimeMillis();
+                            holder.mTxt.setText((endTime - startTime) + "ms");
+                        }
+                    });
+        }
+        return convertView;
+    }
+
+    class ViewHolder {
+        ImageView mImg;
+        TextView mTxt;
+
+    }
+
+}
--- app/src/main/java/cn/chaobao/androidmathematicalart/MainActivity.java
+++ app/src/main/java/cn/chaobao/androidmathematicalart/MainActivity.java
@@ -1,26 +1,20 @@
 package cn.chaobao.androidmathematicalart;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
-
-import java.io.File;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView v= (ImageView) findViewById(R.id.image);
-        ImageView sdv= (ImageView) findViewById(R.id.sd_image);
-        Bitmap bm=MathematicalArt.jniGetMathematicalArt(Environment.getExternalStorageDirectory()
-                + File.separator + "MathPic.jpg", 0);
-        v.setImageBitmap(bm);
-//        sdv.setImageURI(Uri.parse(Environment.getExternalStorageDirectory()
-//                + File.separator + "MathPic.jpg"));
+        mList = (ListView) findViewById(R.id.list);
+        ImageListAdapter adapter = new ImageListAdapter(this, 30);
+        mList.setAdapter(adapter);
+
     }
 
 }
